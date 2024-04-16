9335ed66019476e4011f23146ca0c19c3fcc5de1
Walter Lopes <virtumine02@gmail.com>
2024-04-16 11:39:50 -0300

    Validação de nulo e quantidade de caracteres

--- Program.cs
+++ Program.cs
@@ -1,2 +1,37 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿internal class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+
+        Console.Write("Digite o CPF:");
+
+        string? cpf = Console.ReadLine();
+        
+        ValidarQuantidade(cpf);
+
+
+
+        static void ValidarQuantidade(string? cpf)
+        {
+            
+            if (cpf == null)
+                Console.WriteLine("Digite um CPF");
+
+            var tamanho = cpf?.Length;
+
+            if (tamanho == 11)
+                Console.WriteLine("Este CPF esta correto? " + cpf);
+
+            else
+            {
+                Console.WriteLine(cpf + " tem " + cpf?.Length + " digitos, tem que ter 11 digito");
+            }
+        }
+
+        static void PrimeiroMetodo(string? cpf)
+        {
+            
+        }
+    }
+}
